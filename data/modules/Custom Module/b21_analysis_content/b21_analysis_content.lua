b616c511c329564fed6bd21b4ac08b022ea3f4bc
B21 <thriplow@gmail.com>
2019-09-01 12:24:55 +0100

    added lift, drag, weight, ballast. Spoilers added decimals.

--- data/modules/Custom Module/b21_analysis_content/b21_analysis_content.lua
+++ data/modules/Custom Module/b21_analysis_content/b21_analysis_content.lua
@@ -5,7 +5,11 @@ components = {
 }
 -- local engn_rpm = globalPropertyf("sim/cockpit2/engine/indicators/engine_speed_rpm[0]", 0)
 
-local fontsize = 30
+local FONTSIZE = 18
+local LINE_SPACING = 18
+local UNITS = "german" -- (knots, knots) vs. "german" (kph, mps), "metric" (mps, mps)
+
+local KG_TO_LBS = 2.20462
 
 local background	= loadImage("resources/tacho_background.png")
 local needle		= loadImage("resources/tacho_needle.png")
@@ -26,19 +30,21 @@ local DATAREF_FLAPS = globalPropertyfa("sim/flightmodel2/wing/flap1_deg")
 -- local sim_cd = globalPropertyf("sim/airfoils/afl_cd")
 -- local sim_cm = globalPropertyf("sim/airfoils/afl_cm")
 local DATAREF_ALPHA = globalPropertyf("sim/flightmodel/position/alpha")
+local DATAREF_WEIGHT_KG = globalPropertyf("sim/flightmodel/weight/m_total")
+local DATAREF_BALLAST_KG = globalPropertyf("sim/flightmodel/weight/m_jettison")
+local DATAREF_LIFT_N = globalPropertyf("sim/flightmodel/forces/lift_path_axis")
+local DATAREF_DRAG_N = globalPropertyf("sim/flightmodel/forces/drag_path_axis")
 
-local units = "german" -- (knots, knots) vs. "german" (kph, mps), "metric" (mps, mps)
-
--- each mouse click into the window will switch the units
+-- each mouse click into the window will switch the UNITS
 function onMouseDown(component, x, y, button, parentX, parentY)
-	if units == "metric"
+	if UNITS == "metric"
 	then
-		units = "german"
-	elseif units == "german"
+		UNITS = "german"
+	elseif UNITS == "german"
 	then
-		units = "uk"
+		UNITS = "uk"
 	else
-		units = "metric"
+		UNITS = "metric"
 	end
 end
 
@@ -53,31 +59,46 @@ function draw()
 	-- sasl.gl.drawLine(20, 20, 50, 50, green)
 
 	local speed_mps = get(DATAREF_SPEED_MPS)
-
 	local total_energy_mps = get(DATAREF_TE_MPS)
+	local weight_kg = get(DATAREF_WEIGHT_KG)
+	local ballast_kg = get(DATAREF_BALLAST_KG)
 
 	local speed_str
 	local sink_str
 	local glide_str
 	local spoilers_str
+	local weight_str
+	local ballast_str
+	local lift_str
+	local drag_str
+	local alpha_str
 
-	if units == "metric"
+	if UNITS == "metric"
 	then
 		speed_str = "Speed m/s: "..tostring(math.floor(speed_mps * 100.0) / 100.0) -- floor/divide to set to 2 decimal places
 		sink_str = "Sink m/s:  "..tostring(math.floor((-total_energy_mps) * 100.0) / 100.0)
-	elseif units == "german"
+		weight_str = "Weight kg: "..tostring(math.floor(weight_kg + 0.5 ))
+		ballast_str = "Ballast kg: "..tostring(math.floor(ballast_kg + 0.5 ))
+	elseif UNITS == "german"
 	then
 		speed_str = "Speed kph: "..tostring(math.floor(speed_mps * 3.6 * 100.0) / 100.0)
 		sink_str = "Sink m/s:  "..tostring(math.floor((-total_energy_mps) * 100.0) / 100.0)
+		weight_str = "Weight kg: "..tostring(math.floor(weight_kg + 0.5 ))
+		ballast_str = "Ballast kg: "..tostring(math.floor(ballast_kg + 0.5 ))
 	else -- "uk"
 		speed_str = "Speed kts: "..tostring(math.floor(speed_mps * 1.94384 * 100.0) / 100.0)
 		sink_str = "Sink kts:  "..tostring(math.floor((-total_energy_mps) * 1.94384 * 100.0) / 100.0)
+		weight_str = "Weight lbs: "..tostring(math.floor(weight_kg * KG_TO_LBS + 0.5))
+		ballast_str = "Ballast lbs: "..tostring(math.floor(ballast_kg  * KG_TO_LBS + 0.5))
 	end
 
 	-- cl_str = "CL: "..tostring(math.floor(get(sim_cl) * 1000.0) / 1000.0)
 	-- cd_str = "CD: "..tostring(math.floor(get(sim_cd) * 1000.0) / 1000.0)
 	-- cm_str = "CM: "..tostring(math.floor(get(sim_cm) * 1000.0) / 1000.0)
 	alpha_str = "Alpha: "..tostring(math.floor(get(DATAREF_ALPHA) * 10.0) / 10.0) -- (degrees) 1 decimal place
+
+	lift_str = "Lift N: "..tostring(math.floor(get(DATAREF_LIFT_N) * 10.0) / 10.0)
+	drag_str = "Drag N: "..tostring(math.floor(get(DATAREF_DRAG_N) * 10.0) / 10.0)
 
 	if (total_energy_mps < -0.1) -- limit to avoid silly readings and divide by zero error on pause
 	then
@@ -86,7 +107,7 @@ function draw()
 		glide_str = "L/D ratio: n/a"
 	end 
 
-	local spoilers_str = "Spoilers:  "..tostring(math.floor(get(DATAREF_SPEEDBRAKES) * 100.0) / 100.0)
+	local spoilers_str = "Spoilers:  "..tostring(math.floor(get(DATAREF_SPEEDBRAKES) * 10000.0) / 10000.0)
 
 	local flaps_str = "Flaps (deg):  "..tostring(math.floor(get(DATAREF_FLAPS,1) * 100.0) / 100.0)
 
@@ -94,11 +115,28 @@ function draw()
 	-- sasl.gl.drawText(font,30,330,cd_str,40,false,false,TEXT_ALIGN_LEFT,white)
 	-- sasl.gl.drawText(font,30,280,cm_str,40,false,false,TEXT_ALIGN_LEFT,white)
 
-	sasl.gl.drawText(font,30,175,alpha_str,fontsize,false,false,TEXT_ALIGN_LEFT,white)
-	sasl.gl.drawText(font,30,145,spoilers_str,fontsize,false,false,TEXT_ALIGN_LEFT,white)
-	sasl.gl.drawText(font,30,115,flaps_str,fontsize,false,false,TEXT_ALIGN_LEFT,white)
-	sasl.gl.drawText(font,30,80,speed_str,fontsize,false,false,TEXT_ALIGN_LEFT,white)
-	sasl.gl.drawText(font,30,45,sink_str,fontsize,false,false,TEXT_ALIGN_LEFT,white)
-	sasl.gl.drawText(font,30,10,glide_str,fontsize,false,false,TEXT_ALIGN_LEFT,white)
+	local y = size[2]
+	local x = 10
+
+	y = y - LINE_SPACING
+	sasl.gl.drawText(font,x,y,ballast_str,FONTSIZE,false,false,TEXT_ALIGN_LEFT,white)
+	y = y - LINE_SPACING
+	sasl.gl.drawText(font,x,y,weight_str,FONTSIZE,false,false,TEXT_ALIGN_LEFT,white)
+	y = y - LINE_SPACING
+	sasl.gl.drawText(font,x,y,lift_str,FONTSIZE,false,false,TEXT_ALIGN_LEFT,white)
+	y = y - LINE_SPACING
+	sasl.gl.drawText(font,x,y,drag_str,FONTSIZE,false,false,TEXT_ALIGN_LEFT,white)
+	y = y - LINE_SPACING
+	sasl.gl.drawText(font,x,y,alpha_str,FONTSIZE,false,false,TEXT_ALIGN_LEFT,white)
+	y = y - LINE_SPACING
+	sasl.gl.drawText(font,x,y,spoilers_str,FONTSIZE,false,false,TEXT_ALIGN_LEFT,white)
+	y = y - LINE_SPACING
+	sasl.gl.drawText(font,x,y,flaps_str,FONTSIZE,false,false,TEXT_ALIGN_LEFT,white)
+	y = y - LINE_SPACING
+	sasl.gl.drawText(font,x,y,speed_str,FONTSIZE,false,false,TEXT_ALIGN_LEFT,white)
+	y = y - LINE_SPACING
+	sasl.gl.drawText(font,x,y,sink_str,FONTSIZE,false,false,TEXT_ALIGN_LEFT,white)
+	y = y - LINE_SPACING
+	sasl.gl.drawText(font,x,y,glide_str,FONTSIZE,false,false,TEXT_ALIGN_LEFT,white)
 	
 end
